a94c65bedae409b96bace062983159601e9d26e4
Luis Mejia <lmejia@gmail.com>
2020-11-24 07:49:04 -0800

    Added uberJar for single jar release.

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ tasks.named("test", Test::class) {
 
 dependencies {
     // Main dependencies.
-//    implementation("com.glitchybyte:gcc:1.0.0")
+    // -- None.
     // Test dependencies.
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
@@ -40,4 +40,30 @@ val mainPackage = "$group.gcc"
 application {
     // Set app entry point.
     mainClass.set("$mainPackage.App")
+}
+
+// Create uber jar at the end of build, after tests.
+tasks.named("build") {
+    finalizedBy("uberJar")
+}
+
+tasks.withType<Jar> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+// Register uber jar task.
+tasks.register("uberJar", Jar::class) {
+    group = "Build"
+    description = "Assembles a jar archive containing everything."
+    archiveClassifier.set("uber")
+    manifest {
+        attributes["Main-Class"] = application.mainClass.get()
+    }
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get()
+                .filter { it.name.endsWith("jar") }
+                .map { zipTree(it) }
+    })
 }
