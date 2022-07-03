370acccd2729c30abe60ffd9fafe06a7e9681f37
Luis Mejia <lmejia@gmail.com>
2022-07-03 09:16:09 -0700

    Added run wrapper.

---
+++ run/run.bat
@@ -0,0 +1,23 @@
+:: Run app!
+
+@echo off
+setlocal
+
+:: Capture script directory.
+set script_dir=%~dp0
+
+:: Capture project to run.
+set project=%1
+
+:: Capture arguments.
+set args=
+shift
+:capture_args
+if not "%~1"=="" set "args=%args% %~1" & shift & goto :capture_args
+
+:: Build and get executable.
+set "gpx=%script_dir%gpx.jar"
+for /f %%i in ('java -jar %gpx% code %project%') do set executable=%%i
+
+:: Run app.
+%executable%%args%
